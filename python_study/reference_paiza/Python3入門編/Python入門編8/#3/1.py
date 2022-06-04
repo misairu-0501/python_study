b090aa823efddcd8cfff412b4161b3ea7507da2f
misairu-0501 <97965980+misairu-0501@users.noreply.github.com>
2022-06-04 09:51:35 +0900

    [Add]2022/6/3学習

---
+++ python_study/reference_paiza/Python3入門編/Python入門編8/#3/1.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# クラスからオブジェクトを作成しよう
+
+class Greeting:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hello(self):
+        print("hello " + self.name)
+
+# この下に、必要な処理を記述します
+greeting_1 = Greeting("paiza")
+greeting_1.say_hello()
